b2d2c3ea4aa88111160019750153ff373e742f2c
Cafe137 <xcafe137@gmail.com>
2022-06-11 15:15:50 +0200

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import { Files, Strings, System } from 'cafe-utility'
+import { copy } from 'fs-extra'
+import { mkdir, rm } from 'fs/promises'
+import { join, resolve } from 'path'
+
+const dirname = `temp-${Strings.randomAlphanumeric(8)}`
+
+const beeDashboardDir = resolve(join(dirname, 'bee-dashboard'))
+const installerDir = resolve(join(dirname, 'desktop-node-installer'))
+
+async function cloneRepository(name, target) {
+    await System.execAsync(`git clone git@github.com:${name}.git --depth 1`, false, true, {
+        cwd: resolve(target)
+    })
+}
+
+async function buildRepository(directory) {
+    await System.execAsync('npm i && npm run build', false, true, {
+        cwd: resolve(directory)
+    })
+}
+
+try {
+    await mkdir(dirname)
+    await cloneRepository('ethersphere/bee-dashboard', dirname)
+    await cloneRepository('ethersphere/desktop-node-installer', dirname)
+    await buildRepository(beeDashboardDir)
+    await buildRepository(installerDir)
+    if (await Files.existsAsync('dist')) {
+        await rm('dist', { recursive: true })
+    }
+    await mkdir('dist/static/installer', { recursive: true })
+    await mkdir('dist/static/dashboard')
+    await copy(join(beeDashboardDir, 'build'), 'dist/static/dashboard')
+    await copy(join(installerDir, 'build'), 'dist/static/installer')
+    await copy('assets', 'dist')
+    await System.execAsync('zip -r static.zip dist/*')
+} finally {
+    await rm(dirname, { recursive: true })
+}
