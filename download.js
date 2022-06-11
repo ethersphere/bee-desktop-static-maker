95d1954b4a6d7b4fb71c4226eaaea8770fd5ea42
Cafe137 <xcafe137@gmail.com>
2022-06-12 01:19:44 +0200

    ci: verify download

---
+++ download.js
@@ -0,0 +1,36 @@
+import { Arrays } from 'cafe-utility'
+import { writeFile } from 'fs/promises'
+import fetch from 'node-fetch'
+
+async function getJson(url) {
+    const response = await fetch(url)
+    const json = await response.json()
+    return json
+}
+
+function makeEndpoints(hash) {
+    return Arrays.initialize(10, i => `https://gateway-proxy-bee-${i}-0.gateway.ethswarm.org/bzz/${hash}/`)
+}
+
+async function downloadFile(url, target, lockWrapper) {
+    const response = await fetch(url)
+    if (response.status !== 200) {
+        throw Error('Bad Status')
+    }
+    const arrayBuffer = await response.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    if (lockWrapper.value) {
+        throw Error('Already Downloaded')
+    }
+    lockWrapper.value = true
+    await writeFile(target, buffer)
+}
+
+const latest = await getJson('https://api.github.com/repos/Cafe137/bee-desktop-static-maker/releases/latest')
+
+const swarmHash = latest.name
+const locked = { value: false }
+const before = Date.now()
+await Promise.any(makeEndpoints(swarmHash).map(x => downloadFile(x, 'static.zip', locked)))
+const delta = Date.now() - before
+console.log('downloaded in', delta, 'ms')
